04b27eba9fffa714104d73b795f9c2808fecfcae
arunbabuc <arunbabuc@gmail.com>
2019-04-22 06:28:16 +0000

    Min requirement for play

--- contract/HawksLottery.sol
+++ contract/HawksLottery.sol
@@ -9,7 +9,7 @@ contract HawksLottery{
     }
 
     function play() public payable{
-        // enforce the min requirement of 0.01 ether to play 
+        // enforce the min requirement of 0.01 ether to play
         require(msg.value >= 0.01 ether);
         players.push(msg.sender);
     }
