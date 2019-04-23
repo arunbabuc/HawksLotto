7fa627995a793a3bbc721efc3c7d102678c291f9
arunbabuc <arunbabuc@gmail.com>
2019-04-23 04:59:50 +0000

    Lottery ready

--- contract/HawksLottery.sol
+++ contract/HawksLottery.sol
@@ -9,8 +9,34 @@ contract HawksLottery{
     }
 
     function play() public payable{
-        // enforce the min requirement of 0.01 ether to play
+        // thats the minimum requirement to participate in the lottery
+        // multiple entries - your chances go high
         require(msg.value >= 0.01 ether);
         players.push(msg.sender);
     }
+
+    // this is just a pseudo random number
+    function randomGen() private view returns (uint) {
+        return uint(keccak256(abi.encode(block.difficulty, now, players)));
+    }
+
+    function pickLucky() public restrictedToOwner {
+        uint winnerIndex = randomGen() % players.length;
+        players[winnerIndex].transfer(this.balance);
+        // create the new player dynamic list
+        players = new address[](0);
+    }
+
+    // modifier example to avoid repeated code
+    // _; replaced with the remaining code of the function
+    modifier restrictedToOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    // may be a function to show the players in the webApp
+    function showPlayers() public view returns (address[]) {
+        return players;
+    }
+
 }
